db5d9584de3a9af82c6819153c33fde41ebd1625
mefarpinto93 <manu.edu.far@gmail.com>
2022-01-20 10:26:02 -0600

    update

--- pruebas_visual.py
+++ pruebas_visual.py
@@ -6,25 +6,34 @@ import tkinter
 from turtle import bgcolor
 from isort import file
 from matplotlib.animation import PillowWriter
+from matplotlib.pyplot import text
 
 import pip
 from sklearn.feature_extraction import image
 
 ventana = tkinter.Tk()
 ventana.geometry("700x500")
-ventana = 
 
 cajatexto = tkinter.Entry(ventana)
 cajatexto.pack(side= tkinter.TOP)
 
-boton1 = tkinter.Button(ventana, text = "Buscar mi Nahual", bg = "blue")
+def apachar_boton1(event):
+    myLabel = Label(ventana2, text="Tu nahual es:", bg="blue")
+    myLabel.pack(side= tkinter.BOTTOM)
+
+def ventana2():
+    ventana2 = Toplevel()
+    ventana2.geometry("350x250")
+
+boton1 = tkinter.Button(ventana, text = "Buscar mi Nahual", bg = "limegreen", command=ventana2)
 boton1.pack(side= tkinter.TOP)
 
-image = PhotoImage(file=r"C:\Users\manue\OneDrive\Escritorio\Python\proyecto_grupo\Nahual-885x500.png")
+image = PhotoImage(file=r"C:\Users\manue\OneDrive\Escritorio\Python\proyecto-y-oxlajuj-backtun\Imagenes\Nahual-885x500.png")
 label = Label(ventana, image=image)
 label.pack()
 
 ventana.mainloop()
 
 
+ventana2.mainloop()
 
